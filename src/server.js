03f617c6fa2050dca4ef83430c967f7a58e99fe6
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-11-09 21:12:53 -0600

    basic server is running

---
+++ src/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.status(200).send("Hello World!");
+});
+
+const start = () => {
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+};
+
+module.exports = { start, app };
