2d817acfad29f6b83aad0d699b55c1b676fb5216
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-11-09 21:30:32 -0600

    404 error handler added. Successful

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const notFound = require('./handlers/404');
 
 require('dotenv').config();
 const PORT = process.env.PORT;
@@ -11,6 +12,8 @@ const app = express();
 app.get('/', (req, res, next) => {
     res.status(200).send("Hello World!");
 });
+
+app.use('*', notFound);
 
 const start = () => {
     app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
